08389b011116bfce8688abbc06c4ec9f72229991
SunnyFluy <119286747+SunnyFluy@users.noreply.github.com>
2022-11-28 08:52:42 +0100

    Update start.c

--- Student Project/start.c
+++ Student Project/start.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 int main() {
-
+      struct datum{
+        int day;
+        int month;
+        int year;
+    };
+      struct student{
+        char firtName[50];
+        char lastName[50];
+        int matrikelNmr;
+    };
 }
 
