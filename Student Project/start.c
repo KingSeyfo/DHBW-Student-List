669cc25d28c13182d4bc0ab6015c20c6d2ad4715
Seyfu <Seyfu@DESKTOP-VS3P011.fritz.box>
2022-11-28 09:56:17 +0100

    Datum Checker Fertig

--- Student Project/start.c
+++ Student Project/start.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-
+void inputDatum();
+bool checkDatum();
 int main(){
-
+	inputDatum();
     return 0;
 }
 
@@ -26,6 +27,7 @@ void inputDatum(){
 	int tag,monat,jahr;
 	printf("Bitte Datum eingeben (tt/mm/jjjj Format): ");
 	scanf("%d/%d/%d",&tag,&monat,&jahr);
+	checkDatum(tag, monat, jahr);
 }
 
 
@@ -33,16 +35,12 @@ bool checkDatum(int tag, int monat, int jahr){
 	if(jahr>=1900 && jahr<=9999){
 		if(monat>=1 && monat<=12){
 			if((tag>=1 && tag<=31) && (monat==1 || monat==3 || monat==5 || monat==7 || monat==8 || monat==10 || monat==12)){
-				printf("Ungültiges Datum.\n");
 				return true;
 			}else if((tag>=1 && tag<=30) && (monat==4 || monat==6 || monat==9 || monat==11)){
-				printf("Ungültiges Datum.\n");
 				return true;
 			}else if((tag>=1 && tag<=28) && (monat==2)){
-				printf("Ungültiges Datum.\n");
 				return true;
 			}else if(tag==29 && monat==2 && (jahr%400==0 ||(jahr%4==0 && jahr%100!=0))){
-				printf("Ungültiges Datum.\n");
 				return true;
 			}else{
 				printf("Ungültiger Tag.\n");
