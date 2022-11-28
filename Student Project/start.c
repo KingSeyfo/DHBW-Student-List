f692d1e049ae6390d802dae22f1db0b37fe07d7e
SunnyFluy <119286747+SunnyFluy@users.noreply.github.com>
2022-11-28 09:20:12 +0100

    Update start.c

--- Student Project/start.c
+++ Student Project/start.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
 
-int main() {
-      struct datum{
-        int dd;
-        int mm;
-        int yyyy;
-            
-        printf("Enter date (DD/MM/YYYY format): ");
-    scanf("%d/%d/%d",&dd,&mm,&yyyy);
+
+int main(){
+    struct datum{
+        int day;
+        int month;
+        int year;
+    };
+    struct student{
+        char firtName[50];
+        char lastName[50];
+        int matrikelNr;
+    };
+
+    int dd,mm,yy;
+     
+    printf("Enter date (DD/MM/YYYY format): ");
+    scanf("%d/%d/%d",&dd,&mm,&yy);
      
     //check year
-    if(yyyy>=1900 && yyyy<=9999)
+    if(yy>=1900 && yy<=9999)
     {
         //check month
         if(mm>=1 && mm<=12)
@@ -22,7 +31,7 @@ int main() {
                 printf("Date is valid.\n");
             else if((dd>=1 && dd<=28) && (mm==2))
                 printf("Date is valid.\n");
-            else if(dd==29 && mm==2 && (yyyy%400==0 ||(yyyy%4==0 && yyyy%100!=0)))
+            else if(dd==29 && mm==2 && (yy%400==0 ||(yy%4==0 && yy%100!=0)))
                 printf("Date is valid.\n");
             else
                 printf("Day is invalid.\n");
@@ -36,14 +45,6 @@ int main() {
     {
         printf("Year is not valid.\n");
     }
-    };
-      struct student{
-        char firtName[50];
-        char lastName[50];
-        int matrikelNr;
-    };
-        return 0;
+ 
+    return 0;    
 }
-     
-    
-
