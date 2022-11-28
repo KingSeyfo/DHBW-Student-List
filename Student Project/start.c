8bb108a561ef91c73bdfb5a71f6359f7b5800979
Seyfu <Seyfu@DESKTOP-VS3P011.fritz.box>
2022-11-28 09:39:55 +0100

    Functions first steps

--- Student Project/start.c
+++ Student Project/start.c
@@ -1,59 +1,59 @@
 #include <stdio.h>
-#include <
+#include <stdbool.h>
 
-int main() {
-      struct datum{
-        int dd;
-        int mm;
-        int yyyy;
-            
-        printf("Enter date (DD/MM/YYYY format): ");
-    scanf("%d/%d/%d",&dd,&mm,&yyyy);
-     
-    //check year
-    if(yyyy>=1900 && yyyy<=9999)
-    {
-        //check month
-        if(mm>=1 && mm<=12)
-        {
-            //check days
-            if((dd>=1 && dd<=31) && (mm==1 || mm==3 || mm==5 || mm==7 || mm==8 || mm==10 || mm==12))
-                printf("Date is valid.\n");
-            else if((dd>=1 && dd<=30) && (mm==4 || mm==6 || mm==9 || mm==11))
-                printf("Date is valid.\n");
-            else if((dd>=1 && dd<=28) && (mm==2))
-                printf("Date is valid.\n");
-            else if(dd==29 && mm==2 && (yyyy%400==0 ||(yyyy%4==0 && yyyy%100!=0)))
-                printf("Date is valid.\n");
-            else
-                printf("Day is invalid.\n");
-        }
-        else
-        {
-            printf("Month is not valid.\n");
-        }
-    }
-    else
-    {
-        printf("Year is not valid.\n");
-    }
-    };
-      struct student{
-        char firtName[50];
-        char lastName[50];
-        int matrikelNr;
-    };
-        return 0;
+
+int main(){
+
+    return 0;
 }
-     
-    
 
-struct studMgr{
-	char name;
-	int matrikelNr;
-}student,students;
-struct date{
-	int day;
-	int month;
-	int year;
+
+struct datum{
+        int day;
+        int month;
+        int year;
 };
+
+
+struct student{
+	char firtName[50];
+	char lastName[50];
+	int matrikelNr;
+};
+
+
+void inputDatum(){
+	int tag,monat,jahr;
+	printf("Bitte Datum eingeben (tt/mm/jjjj Format): ");
+	scanf("%d/%d/%d",&tag,&monat,&jahr);
+}
+
+
+bool checkDatum(int tag, int monat, int jahr){
+	if(jahr>=1900 && jahr<=9999){
+		if(monat>=1 && monat<=12){
+			if((tag>=1 && tag<=31) && (monat==1 || monat==3 || monat==5 || monat==7 || monat==8 || monat==10 || monat==12)){
+				printf("Ungültiges Datum.\n");
+				return true;
+			}else if((tag>=1 && tag<=30) && (monat==4 || monat==6 || monat==9 || monat==11)){
+				printf("Ungültiges Datum.\n");
+				return true;
+			}else if((tag>=1 && tag<=28) && (monat==2)){
+				printf("Ungültiges Datum.\n");
+				return true;
+			}else if(tag==29 && monat==2 && (jahr%400==0 ||(jahr%4==0 && jahr%100!=0))){
+				printf("Ungültiges Datum.\n");
+				return true;
+			}else{
+				printf("Ungültiger Tag.\n");
+				return false;
+			}
+		}else{
+			printf("Ungültiger Monat.\n");
+			return false;
+		}
+	}else{
+		printf("Ungültiges Jahr.\n");
+		return false;
+	}
+}
