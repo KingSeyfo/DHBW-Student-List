154c84b74d32edbd6410bd870c964186afa25ea8
SunnyFluy <119286747+SunnyFluy@users.noreply.github.com>
2022-12-24 15:37:43 +0100

    Update start.c

--- Student Project/start.c
+++ Student Project/start.c
@@ -1,18 +1,94 @@
 #include <stdio.h>
 
+//Ausflistung aller Funktionen, kann selbstständig oder bei falschem Input aufgerufen werden.
+menue(){
+    printf("Auflistung moeglicher Funktionen:\n----------------------------------\n");
+    printf("menue();                         Diese Funktion listet alle moeglichen Funktionen auf.\n");
+    printf("addStudent();                    Diese Funktion erstellt einen neuen Studenten mit den eingetragenen Werten.\n");
+    printf("inputStudent();                  Diese Funktion aendert die Daten eines Studenten.\n");
+    printf("printStudent(Matrikelnummer);    Diese Funktion gibt alle Daten eines Studenten aus.\n");
+    printf("studentCount();                  Diese Funktion gibt die Anzahl aller Stundenten aus.\n");
+    printf("printAllStudents();              Diese Funktion gibt sortiert die Daten aller Studenten aus.\n");
+    printf("deleteStundent(Matrikelnummer);  Diese Funktion löscht einen Studenten und seine Daten.\n");
+}
 
-int main(){
-    struct datum{
-        int day;
-        int month;
-        int year;
-    };
-    struct student{
-        char firtName[50];
-        char lastName[50];
-        int matrikelNr;
-    };
+addStudent(){
+    // student new Student
+}
 
+//Abändern der Daten eines Studenten
+inputStudent(){
+    char pNachname[50];
+    printf("Enter new Nachname");
+    scanf("%c",&pNachname);
+    //student.nachname = pNachname;
+
+    int pMatrikelnummer;
+    printf("Enter new Matrikelnummer");
+    scanf("%d",&pMatrikelnummer);
+    //student.matrikelnummer = pMatrikelnummer;
+
+    //datum pStartdatum;
+    printf("Enter new Startdatum (Format:dd/mm/yy)");
+    //scanf("%d/%d/%d",&),&pStartdatum.day,&pStartdatum.month,&pStartdatum.year); // mit Datumscheck
+    //student.startdatum = pStartdatum;
+
+    //datum pEnddatum;
+    printf("Enter new Enddatum (Format:dd/mm/yy)");
+    //scanf("%d/%d/%d",&),&pEnddatum.day,&pEnddatum.month,&pEnddatum.year); // mit Datumscheck
+    //student.enddatum = pEnddatum;
+
+    //datum pGeburtsdatum;
+    printf("Enter new Geburtsdatum (Format:dd/mm/yy)");
+    //scanf("%d/%d/%d",&),&pGeburtsdatum.day,&pGeburtsdatum.month,&pGeburtsdatum.year); // mit Datumscheck
+    //student.geburtsdatum = pGeburtsdatum;
+}
+
+printStudent(int pMatrikelnummer){
+    //suche nach student.matrikelnummer == pMatrikelnummer, falls nicht existiert error ausspucken
+    printf("Nachname:%c\nMatrikelnummer:%d\nStartdatum:%d/%d/%d");
+    //printf("Matrikelnummer:%d\n",&student.matrikelnummer);
+    //printf("Startdatum:%\n", );
+    //printf("Enddatum:%\n", );
+    //printf("Geburtsdatum:%\n", );
+}
+
+studentCount(){
+    int i = 0;
+    // while (student.next != NULL){
+    //    student = student.next;
+    //    i++;
+    //}
+    printf("Die Anzahl der Stundenten betraegt:%d\n",&i);
+}
+
+printAllStundents(){
+    printf("Daten aller Stundenten:\n");
+    //spätestens jetzt die Studentenliste sortieren
+    //while(student.next != NULL){
+    //    student = student.next;
+    //    printf("%c, %d, %, % ,%\n"student.nachname,student.matrikelnummer,student.startdatum,student.enddatum,student.geburtstag);
+    //}
+}
+
+deleteStudent(int pMatrikelnummer){
+    //suche nach student.matrikelnummer == pMatrikelnummer
+    //zeiger des studentenvorher zeigt auf student.next
+    //danach element student löschen
+}
+
+/*save(){
+    bei beendung des Programms soll eine Datei erzeugt werden
+    in der alle Studenten gespeichert werden sollen 
+}
+
+  read(){
+    bei start des Programms soll die Datei ausgelesen werden 
+    und aus den Daten eine neue Liste erstellt werden.
+  }
+*/
+
+checkDate(){
     int dd,mm,yy;
      
     printf("Enter date (DD/MM/YYYY format): ");
@@ -40,4 +116,21 @@ int main(){
     else{
         printf("Day is invalid.\n");}
     return 0;    
+}
+
+
+int main(){
+    struct date{
+        int day;
+        int month;
+        int year;
+    };
+    struct student{
+        char nachname[50];
+        int matrikelnummer;
+        //date startdatum;
+        //date enddatum;
+        //date geburtsdatum;
+    };
+
 }
