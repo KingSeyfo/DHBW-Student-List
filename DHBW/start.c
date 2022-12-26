bf88e0c1fbfc53a2501b268a4d421b7ba9da55b3
Seyfu <Seyfu@DESKTOP-HHH3V6O>
2022-12-26 02:48:31 +0100

    int to array fehlt

--- DHBW/start.c
+++ DHBW/start.c
@@ -11,6 +11,9 @@ typedef struct student Student;
 
 void chooseMode();
 void addStudent(FILE* file);
+void delete(FILE* file);
+void deleteStudent(FILE* file, int matrikel);
+void scanLinesforArray(FILE* file,char search[],int* ptr);
 void combine(char (*fname)[20], char (*lname)[20], int* id, Datum* geb, Datum* start, Datum* end, Student* final);
 void inputStudent(char (*fname)[20], char (*lname)[20], int* id, Datum* geb, Datum* start, Datum* end);
 void inputData(char msg[30], char (*ptr)[20]);
@@ -72,6 +75,10 @@ void chooseMode(){
 		chooseMode();
 		break;
 	case 3:
+		file = fopen("C:\\Users\\Seyfu\\Desktop\\students.txt", "r+");
+		delete(file);
+		fclose(file);
+		chooseMode();
 		break;
 	case 4:
 		break;
@@ -88,8 +95,33 @@ void chooseMode(){
 }
 
 
-void deleteStudent(int matrikel){
+void delete(FILE* file){
+	int matrikel,*ptr;
+	ptr = &matrikel;
+	printf("Wen möchtest du Löschen?\n");
+	printf("Bitte Matrikelnummer angeben:\n");
+	scanf("%d",ptr);
+	deleteStudent(file, matrikel);
+}
 
+
+void deleteStudent(FILE* file, int matrikel){
+	 int line,*lineptr;
+	 lineptr = &line;
+	 scanLinesforArray(file, ((char)matrikel), lineptr);
+}
+
+
+void scanLinesforArray(FILE* file, char search[], int* lineNr){
+	char line[20] = {0};
+	int line_count = 0;
+	while(fgets(line, 20,file)){
+		++line_count;
+		if(strcmp(line,search) == 0){
+			//int to string conversion
+			printf("SUCCESS WITH LINE %d\n",(char)line_count);
+		}
+	}
 }
 
 
